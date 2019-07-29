4a15e8cdd9451845f6cfe7202aa16bbb079df99c
GodfroidEmmanuel <godfroid.emmanuel@gmail.com>
2019-07-29 15:44:16 +0200

    MardownTranslator

---
+++ build/precache-manifest.b0b13346cb5082a09c7030a13dbd9fe9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d69c1782e9bc662564149c383ac6aef",
+    "url": "/markdown-translator/index.html"
+  },
+  {
+    "revision": "845333e1abaf5d02c1c4",
+    "url": "/markdown-translator/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "2185376ede92ff2549f4",
+    "url": "/markdown-translator/static/css/main.f24b6848.chunk.css"
+  },
+  {
+    "revision": "845333e1abaf5d02c1c4",
+    "url": "/markdown-translator/static/js/2.9b5dead0.chunk.js"
+  },
+  {
+    "revision": "2185376ede92ff2549f4",
+    "url": "/markdown-translator/static/js/main.203f44be.chunk.js"
+  },
+  {
+    "revision": "c4406df1986c28ad88d9",
+    "url": "/markdown-translator/static/js/runtime~main.9294a209.js"
+  }
+]);
